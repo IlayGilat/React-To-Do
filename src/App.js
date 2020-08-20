8b755d4b18781a2611b5bf1b1d98ae0c8f648861
ilay gilat <31382340+IlayGilat@users.noreply.github.com>
2020-08-20 11:08:16 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import React from 'react'
-import ToDoList from './ToDoList'
-import Settings from './Settings'
-class App extends React.Component{
+import React from "react";
+import ToDoList from "./ToDoList";
+import Settings from "./Settings";
+class App extends React.Component {
+  state = {
+    labelname: "",
+  };
 
-  
-    render(){
-        return (
-            <div>
-            <Settings/>
-                <ToDoList/>
-            </div>
-        )
-    }
+  OnFormSubmit = async (state) => {
+    await this.setState({
+      labelname: state.labelname,
+    });
+    //console.log(this.state.labelname);
+  };
+  render() {
+    return (
+      <div>
+        <Settings OnFormSubmit={this.OnFormSubmit} />
+        <ToDoList AddLabel={this.state.labelname} />
+      </div>
+    );
+  }
 }
 
-export default App 
+export default App;
